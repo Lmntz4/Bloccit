ec40f8fa8b7feacfb24d57512935158a8748225c
Jeff Rogers <lmntz23@gmail.com>
2015-05-09 18:38:39 +0000

    Assignment 29 more CRUD fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
   end
   
   def create
-    @question = Question.new(params.require(:question).permit(:title, :body))
+    @question = Question.new(question_params)
     if @question.save
       flash[:notice] = "Question was saved."
       redirect_to @question
@@ -25,4 +25,27 @@ class QuestionsController < ApplicationController
   def edit
     @question = Question.find(params[:id])
   end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+end
+
+private
+
+def question_params
+  params.require(:question).permit(:title, :body, :resolved)
 end
