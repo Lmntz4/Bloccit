2915df7eb91c0bf8272022d564d57db15b5f70dc
Jeff Rogers <lmntz23@gmail.com>
2015-05-14 10:56:13 +0000

    Progress on Checkpoint 38, Uploading Images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512050453) do
+ActiveRecord::Schema.define(version: 20150514081819) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20150512050453) do
     t.datetime "updated_at"
   end
 
+  create_table "summaries", force: true do |t|
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
+
   create_table "topics", force: true do |t|
     t.string   "name"
     t.boolean  "public",      default: true
@@ -69,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150512050453) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
