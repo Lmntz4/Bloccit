5a3b03f624e1e152fb1ec8462e9078630af661ac
Jeff Rogers <lmntz23@gmail.com>
2015-05-28 01:19:16 +0000

    Checkpoint Favoriting, Completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,10 @@ class CommentsController < ApplicationController
     
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,16 @@ class User < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
-def admin?
-  role == 'admin'
-end
+  def admin?
+    role == 'admin'
+  end
 
-def moderator?
-  role == 'moderator'
-end
+  def moderator?
+    role == 'moderator'
+  end
+
+  def favorited(post)
+    favorites.where(post_id: post.id).first
+  end
 
 end
