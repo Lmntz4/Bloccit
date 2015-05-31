9c316324ab0979ed273fd277911806e1f1e9c2bc
Jeff Rogers <lmntz23@gmail.com>
2015-05-31 05:24:09 +0000

    Progress on Topics and Posts Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'summaries/create'
-
-  get 'summaries/show'
-
   devise_for :users
 
   resources :topics do
   resources :posts, except: [:index]
-    resource :summary
   end
    
   get 'about' => 'welcome#about'
