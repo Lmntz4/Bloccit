b3ffaefaa1c590d1d1f98c57d0c49a1bb473b326
Jeff Rogers <lmntz23@gmail.com>
2015-06-02 00:37:04 +0000

    Added populate_summary to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
   has_one :summary
   
   default_scope { order('created_at DESC') }
+  
+  before_save :populate_summary
+  
+  private 
+  
+  def populate_summary
+    if summary.nil? and @body.present?
+      summary = @body.slice(0,50)
+    end
+  end
 end
