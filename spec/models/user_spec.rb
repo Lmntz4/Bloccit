0a9553a209fc68647811be00f3db8fdddc0c07dd
Jeff Rogers <lmntz23@gmail.com>
2015-06-02 09:22:35 +0000

    Progress on Assignment, Testing Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,9 +32,8 @@ describe User do
     before do
       @user1 = create(:user_with_post_and_comment)
 
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post) }
+      @user2 = create(:user_with_post_and_comment)
+      
     end
 
     it "returns users ordered by comments + posts" do
