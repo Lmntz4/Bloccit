7b2facc748e9c7b0d2c3e8df48fb34d5b0bb90b6
Jeff Rogers <lmntz23@gmail.com>
2015-06-02 10:09:48 +0000

    Assignment, Testing Users, Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,11 +33,17 @@ describe User do
       @user1 = create(:user_with_post_and_comment)
 
       @user2 = create(:user_with_post_and_comment)
+      create(:comment, user: @user2)
       
     end
 
     it "returns users ordered by comments + posts" do
       expect( User.top_rated).to eq([@user2, @user1])
+    end
+    
+    it "stores a 'posts_count' on user" do
+      users = User.top_rated
+      expect( users.first.posts_count ).to eq(1)
     end
 
     it "stores a 'comments_count' on user" do
