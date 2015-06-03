155ea19f3d92bcfb1ba90da86e9cd59ceb9eadc6
Jeff Rogers <lmntz23@gmail.com>
2015-06-03 03:57:33 +0000

    Fixed, Creating and Destroying with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,47 @@
 class CommentsController < ApplicationController
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)
+    @comments = @post.comments
+
+    @comment = current_user.comments.build( comment_params )
     @comment.post = @post
-    @new_comment = Comment.new
-    
+
     authorize @comment
-    
+
     if @comment.save
-      flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
+      flash[:notice] = "Comment was created."
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @topic = Topic.find(@post.topic)
     @comment = @post.comments.find(params[:id])
-
     authorize @comment
-    
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
-      flash[:error] = "Comment wasn't removed. Try again."
-      redirect_to [@topic, @post]
+      flash[:error] = "Comment couldn't be deleted. Try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body)
   end
+
 end
