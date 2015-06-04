0cf53383bec582af9ef0b23eadbff49ef09c9661
Jeff Rogers <lmntz23@gmail.com>
2015-06-04 22:14:10 +0000

    Stuck on Summary assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
   def show
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    @summary = @post.summary
   end
 
   def new
--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,12 +1,8 @@
 class SummariesController < ApplicationController
     
   def show
-    @topic = Topic.find(params[:topic_id])
-    @posts = @topic.posts
-    @summary = Summary.find(params[:id])
   end
     
   def create
-    
   end
 end
